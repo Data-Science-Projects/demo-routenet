b7ccebb5acdf3b08e788e85c5814ac772dc873b3
Nathan Sowatskey <nathan@nathan.to>
2019-09-28 19:02:40 +0200

    Fixed train_multiple.py.

--- bin/train_multiple.py
+++ bin/train_multiple.py
@@ -7,6 +7,7 @@ synth50bw data sets.
 
 import glob
 import os
+import shutil
 
 from routenet.train import train as rn_train
 
@@ -14,14 +15,20 @@ omnet_data_dir = os.getenv('OMNET_DATA_DIR')
 
 nsfnetbw_train_files_list = glob.glob(omnet_data_dir + '/nsfnetbw/tfrecords/train/*.tfrecords')
 synth50bw_train_files_list = glob.glob(omnet_data_dir + '/synth50bw/tfrecords/train/*.tfrecords')
+train_files_list = nsfnetbw_train_files_list + synth50bw_train_files_list
 
 nsfnetbw_eval_files_list = glob.glob(omnet_data_dir + '/nsfnetbw/tfrecords/evaluate/*.tfrecords')
 synth50bw_eval_files_list = glob.glob(omnet_data_dir + '/synth50bw/tfrecords/evaluate/*.tfrecords')
+eval_files_list = nsfnetbw_eval_files_list + synth50bw_eval_files_list
 
-rn_train.train_and_evaluate(model_dir='../trained_models',
-                            train_files=nsfnetbw_train_files_list + synth50bw_train_files_list,
+model_chkpnt_dir = '../model_checkpoints/'
+
+shutil.rmtree(model_chkpnt_dir, ignore_errors=True)
+
+rn_train.train_and_evaluate(model_dir=model_chkpnt_dir,
+                            train_files=train_files_list,
                             shuffle_buf=30,
                             target='delay',
                             train_steps=100,
-                            eval_files=nsfnetbw_eval_files_list + synth50bw_eval_files_list,
+                            eval_files=eval_files_list,
                             warm_start_from=None)
