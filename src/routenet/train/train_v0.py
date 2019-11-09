Fixing test after backing out normalisation.

src/routenet/train/train_v0.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2019, Krzysztof Rusek [^1], Paul Almasan [^2], Nathan Sowatskey, Ana Matute.
+# Copyright (c) 2019, Krzysztof Rusek [^1], Paul Almasan [^2]
 #
 # [^1]: AGH University of Science and Technology, Department of
 #     communications, Krakow, Poland. Email: krusek\@agh.edu.pl
@@ -9,7 +9,6 @@
 
 """
 The functions here execute the training steps for the RouteNetModel.
-TODO refactor as a class to imprice handling of global normvals
 """
 
 from __future__ import print_function
@@ -19,14 +18,11 @@ import sys
 import tensorflow as tf
 
 from routenet.model.routenet_model_v0 import RouteNetModelV0
-from routenet.train.normvals_v0 import NormVals
 
 rn_default_checkpointing_config = tf.estimator.RunConfig(
     save_checkpoints_secs=10 * 60,  # Save checkpoints every 10 minutes
     keep_checkpoint_max=20  # Retain the 20 most recent checkpoints.
 )
-
-default_normvals = NormVals()
 
 
 def train_and_evaluate(model_dir,
@@ -37,8 +33,7 @@ def train_and_evaluate(model_dir,
                        eval_files,
                        warm_start_from,
                        model_hparams=RouteNetModelV0.default_hparams,
-                       checkpointing_config=rn_default_checkpointing_config,
-                       norm_vals=default_normvals):
+                       checkpointing_config=rn_default_checkpointing_config):
 
     estimator = tf.estimator.Estimator(model_fn=model_fn,
                                        model_dir=model_dir,
@@ -50,16 +45,14 @@ def train_and_evaluate(model_dir,
                                         lambda: tfrecord_input_fn(filenames=train_files,
                                                                   hparams=model_hparams,
                                                                   shuffle_buf=shuffle_buf,
-                                                                  target=target,
-                                                                  norm_vals=norm_vals),
+                                                                  target=target),
                                         max_steps=train_steps)
 
     eval_spec = tf.estimator.EvalSpec(input_fn=
                                       lambda: tfrecord_input_fn(filenames=eval_files,
                                                                 hparams=model_hparams,
-                                                                shuffle_buf=None,  # TODO None?
-                                                                target=target,
-                                                                norm_vals=norm_vals),
+                                                                shuffle_buf=None,
+                                                                target=target),
                                       throttle_secs=10 * 60)
 
     tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
@@ -100,8 +93,7 @@ def transformation_func(itrtr, batch_size=32):
     return tensors
 
 
-def tfrecord_input_fn(filenames, hparams, shuffle_buf=1000, target='delay',
-                      norm_vals=default_normvals):
+def tfrecord_input_fn(filenames, hparams, shuffle_buf=1000, target='delay'):
 
     files = tf.data.Dataset.from_tensor_slices(filenames)
     files = files.shuffle(len(filenames))
@@ -117,7 +109,7 @@ def tfrecord_input_fn(filenames, hparams, shuffle_buf=1000, target='delay',
         dataset = dataset.shuffle(shuffle_buf, reshuffle_each_iteration=True).repeat()
 
     # TODO constants 2 and 10 should be externalised
-    dataset = dataset.map(lambda buf: parse(buf, target, norm_vals), num_parallel_calls=2)
+    dataset = dataset.map(lambda buf: parse(buf, target), num_parallel_calls=2)
     dataset = dataset.prefetch(10)
 
     # TODO, remove iterator and pass data set directly
@@ -208,7 +200,7 @@ def model_fn(features, labels, mode, params):
                                       training_hooks=[logging_hook])
 
 
-def parse(serialized, target='delay', norm_vals=default_normvals):
+def parse(serialized, target='delay'):
     # Target is the name of predicted variable
     # TODO 'traffic' below is bandwidth of traffic transmitted
     with tf.device('/cpu:0'):
@@ -229,16 +221,15 @@ def parse(serialized, target='delay', norm_vals=default_normvals):
                                                                                              tf.int64)})
 
             for feature in ['traffic', target, 'link_capacity', 'links', 'paths', 'sequences']:
+                # This is normalisation for the form val-mean/std dev..
                 features[feature] = tf.sparse.to_dense(features[feature])
+                # TODO create switch for delay normalisation
                 if feature == 'delay':
-                    features[feature] = ((features[feature] - norm_vals.mean_delay) /
-                                         norm_vals.std_delay)
+                    features[feature] = (features[feature] - 0.37) / 0.54
                 if feature == 'traffic':
-                    features[feature] = ((features[feature] - norm_vals.mean_traffic) /
-                                         norm_vals.std_traffic)
+                    features[feature] = (features[feature] - 0.17) / 0.13
                 if feature == 'link_capacity':
-                    features[feature] = ((features[feature] - norm_vals.mean_link_capacity) /
-                                         norm_vals.std_link_capacity)
+                    features[feature] = (features[feature] - 25.0) / 40.0
 
     return {k: v for k, v in features.items() if k is not target}, features[target]
 

tests/utils/test_utils.py
@@ -7,9 +7,10 @@ import tensorflow as tf
 from sklearn.metrics import mean_squared_error, r2_score
 
 from routenet.model.routenet_model_v0 import RouteNetModelV0
-from routenet.train.normvals_v0 import NormVals
 from routenet.train.train_v0 import read_dataset
 
+
+# TODO This code should be refactored with the code in the rn_notebook_utils.py
 
 def get_model_readout(test_sample_file):
     graph = tf.Graph()
@@ -40,7 +41,7 @@ def get_model_readout(test_sample_file):
 
 
 def run_predictions(graph, readout, data_set_itrtr, labels, checkpoint_id, checkpoint_dir,
-                    norm_vals=NormVals()):
+                    normalise_pred=True):
 
     with tf.compat.v1.Session(graph=graph) as sess:
         sess.run(tf.compat.v1.local_variables_initializer())
@@ -64,27 +65,25 @@ def run_predictions(graph, readout, data_set_itrtr, labels, checkpoint_id, check
             # Note that we need to pass back the median of the `pred_delay` and the true_delay
             # just so that we have two tensors of the same shape for graphing purposes.
             predicted, true_vals = sess.run([readout, labels])
-            predicted = norm_vals.std_delay * predicted + norm_vals.mean_delay
+            if normalise_pred:
+                predicted = 0.54 * predicted + 0.37
             predictions.append(predicted)
 
         median_prediction = np.median(predictions, axis=0)
 
-        true_vals = norm_vals.std_delay * true_vals + norm_vals.mean_delay
+        if normalise_pred:
+            true_vals = 0.54 * true_vals + 0.37
 
     mse = mean_squared_error(median_prediction, true_vals[0])
     r2 = r2_score(median_prediction, true_vals[0])
     return median_prediction, predictions, true_vals, mse, r2
 
 
-def do_test_prediction(sample_file, checkpoint_dir, checkpoint_id=50000, norm_vals=NormVals()):
+def do_test_prediction(sample_file, checkpoint_dir, checkpoint_id=50000, normalise_pred=True):
     graph, readout, data_set_itrtr, labels = get_model_readout(sample_file)
-    _, _, _, mse, r2 = run_predictions(graph,
-                                       readout,
-                                       data_set_itrtr,
-                                       labels,
-                                       checkpoint_id,
+    _, _, _, mse, r2 = run_predictions(graph, readout, data_set_itrtr, labels, checkpoint_id,
                                        checkpoint_dir,
-                                       norm_vals=norm_vals)
+                                       normalise_pred=True)
 
     return mse, r2
 